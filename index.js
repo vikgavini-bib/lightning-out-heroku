6401bd79e55b25a366308112f76cbb9344c5f4fb
Vik Gavini <vikram.gavini1_ext@michelin.com>
2020-05-22 10:37:13 -0700

    Adding nav changes

--- index.js
+++ index.js
@@ -15,18 +15,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', cors(), (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-express()
-  .use(function(req, res, next) {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization');
-      res.setHeader('Access-Control-Allow-Methods', '*');
-      res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
-      res.setHeader('Access-Control-Max-Age', '1000');
-      next();
-  })
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
   .get('/auth', cors(), (req, res) => res.render('pages/auth'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
