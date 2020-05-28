ace06336dba5a43fec1c96fe54c9394fdec14716
Vik Gavini <vikram.gavini1_ext@michelin.com>
2020-05-28 08:48:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(function(req, res, next) {
-      //res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization');
       res.setHeader('Access-Control-Allow-Methods', '*');
       res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
